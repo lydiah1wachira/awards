3a6ec1d6ed1652cd44548f82974ce42fa1a462b2
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-13 23:58:59 +0300

    create a contacts email field for profile model.

--- awards/models.py
+++ awards/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     profile_pic=models.ImageField(upload_to = 'profile/')
+    name = models.CharField(blank=True, max_length=120)
     bio=models.CharField(max_length=60)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    phone=models.IntegerField()
-    name = models.CharField(blank=True, max_length=120)
+    contact=models.EmailField(max_length=100, blank=True)
+   
     
