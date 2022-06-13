c6102680409e69e817d82e8dc738f7833b249c54
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-14 00:25:54 +0300

    update index view to display all projects.

--- awards/views.py
+++ awards/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render,redirect
+from .models import Profile,Project
+from django.http import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
 
 # Create your views here.
 def index(request):
   '''View function to display the index page and its data'''
   
-  return render(request, 'index.html')
+  try:
+    projects = Project.objects.all()
+  except Exception as e :
+    raise  Http404()
+      
+  return render(request, 'index.html', {"projects":projects})
