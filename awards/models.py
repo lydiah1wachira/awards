6e385636f70c89ccb28677a6bdd2781e6a565800
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-13 23:43:56 +0300

    create a profile model.

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic=models.ImageField(upload_to = 'profile/')
+    bio=models.CharField(max_length=60)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    phone=models.IntegerField()
+    class Meta:
+        ordering=['-profile']
