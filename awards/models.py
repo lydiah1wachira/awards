48094f8cee6b29f2536bbafb3bb7623e596d378a
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-14 00:03:21 +0300

    create methods to save the user profile.

--- awards/models.py
+++ awards/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,5 +10,17 @@ class Profile(models.Model):
     bio=models.CharField(max_length=60)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     contact=models.EmailField(max_length=100, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
    
     
