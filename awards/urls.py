1bd931c090b3031463be3aa826b3bc5d8b7dc45d
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-14 15:54:11 +0300

    make migrations for the comments model.

--- awards/urls.py
+++ awards/urls.py
@@ -2,11 +2,11 @@ from . import views
 from django.urls import path,re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views 
+
 
 urlpatterns = [
   path('', views.index, name="index"),
-  path('logout/', views.logout, {"next_page": '/'}),
+
   
 ]
 
