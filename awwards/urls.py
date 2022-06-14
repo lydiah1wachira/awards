0acc0479462e079bc0780c18228cf328b6187954
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-14 15:40:30 +0300

    create a navbar and include it in the base file.

--- awwards/urls.py
+++ awwards/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('awards/',include('awards.urls'))
+    path('awards/',include('awards.urls')),
+    path('accounts/', include('registration.backends.default.urls')),
 ]
 
 if settings.DEBUG:
